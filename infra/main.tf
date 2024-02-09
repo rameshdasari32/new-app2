30f93df08dc2601acb7624e658ab28dadb538bf1
Ramesh Dasari <dr.babu265@gmail.com>
2024-02-09 18:59:25 +0530

    first commit

--- infra/main.tf
+++ infra/main.tf
@@ -13,14 +13,14 @@ module "networking" {
   eu_availability_zone = var.eu_availability_zone
   cidr_private_subnet  = var.cidr_private_subnet
 }
-
+/*
 module "security_group" {
   source                     = "./security-groups"
   ec2_sg_name                = "SG for EC2 to enable SSH(22) and HTTP(80)"
   vpc_id                     = module.networking.dev_proj_1_vpc_id
   public_subnet_cidr_block   = tolist(module.networking.public_subnet_cidr_block)
   ec2_sg_name_for_python_api = "SG for EC2 for enabling port 5000"
-} /*
+} 
 
 module "ec2" {
   source                   = "./ec2"
